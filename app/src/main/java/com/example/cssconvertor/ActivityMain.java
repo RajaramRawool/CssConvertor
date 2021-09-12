bfbf014f11fd65fba43a70217093ce9f6b3b540c
Rajaram Rawool <rajaram903@gamil.com>
2021-09-12 15:05:38 +0530

    3 dynamic layout

--- app/src/main/java/com/example/cssconvertor/ActivityMain.java
+++ app/src/main/java/com/example/cssconvertor/ActivityMain.java
@@ -3,18 +3,17 @@ package com.example.cssconvertor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActivityMain extends AppCompatActivity {
-    EditText etPixels;
-    EditText etEm;
-    EditText etPoints;
-    Button btCalculate, btPixels, btEms, btPoints;
+    EditText etPixels, etEms, etPoints;
+    TextView tvPxToEm, tvPxToPt, tvEmToPx, tvEmToPt, tvPtToPx, tvPtToEm;
+    Button btConvert, btPixels, btEms, btPoints;
     LinearLayout llConvertor ;
 
     @Override
@@ -27,10 +26,12 @@ public class ActivityMain extends AppCompatActivity {
     }
 
     private void setListener() {
-        btCalculate.setOnClickListener(new View.OnClickListener() {
+        btConvert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validateUserInput();
+
+//                Toast.makeText(ActivityMain.this,"Hi!",Toast.LENGTH_SHORT).show();
+
             }
         });
 
@@ -38,24 +39,47 @@ public class ActivityMain extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 View view = getLayoutInflater().inflate(R.layout.layout_px_convertor, null, false );
+                llConvertor.removeAllViews();
                 llConvertor.addView(view);
-
-
+            }
+        });
+        btEms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view = getLayoutInflater().inflate(R.layout.layout_ems_convertor,null, false);
+                llConvertor.removeAllViews();
+                llConvertor.addView(view);
             }
         });
 
-    }
-
-    private void validateUserInput() {
-
+        btPoints.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view = getLayoutInflater().inflate(R.layout.layout_points_convertor,null, false);
+                llConvertor.removeAllViews();
+                llConvertor.addView(view);
+            }
+        });
     }
 
     private void setViews() {
-
-        btCalculate = findViewById(R.id.calculate);
+//        EditText
+        etPixels = findViewById(R.id.px_pixels);
+        etEms = findViewById(R.id.ems_em);
+        etPoints = findViewById(R.id.points_points);
+//        TextViews
+        tvPxToEm = findViewById(R.id.px_em);
+        tvPxToPt = findViewById(R.id.px_points);
+        tvEmToPx = findViewById(R.id.ems_pixels);
+        tvEmToPt = findViewById(R.id.ems_points);
+        tvPtToPx = findViewById(R.id.points_pixels);
+        tvPtToEm = findViewById(R.id.points_em);
+//        Buttons
+        btConvert = findViewById(R.id.convert);
         btPixels = findViewById(R.id.button_pixels);
         btEms = findViewById(R.id.button_ems);
         btPoints = findViewById(R.id.button_points);
+//        LinearLayout
         llConvertor = findViewById(R.id.layout_convertor);
     }
 }
